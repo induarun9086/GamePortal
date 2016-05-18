2c76451a241578379bf46044e51fadb2300383fc
Indumathi Duraipandian <indumathi.duraipandian@student.fh-kiel.de>
2016-05-18 09:40:19 +0200

    recursive blocks

--- js/tetris.js
+++ js/tetris.js
@@ -1,3 +1,16 @@
+var colors = [ '#32a4fa', '#38C44F', '#FFAC1C', '#FF6600', '#CC54C4', '#999',
+		'#FF0000' ];
+var puzzles = [ [ [ 0, 0, 1 ], [ 1, 1, 1 ], [ 0, 0, 0 ] ],
+		[ [ 1, 0, 0 ], [ 1, 1, 1 ], [ 0, 0, 0 ] ],
+		[ [ 0, 1, 1 ], [ 1, 1, 0 ], [ 0, 0, 0 ] ],
+		[ [ 1, 1, 0 ], [ 0, 1, 1 ], [ 0, 0, 0 ] ],
+		[ [ 0, 1, 0 ], [ 1, 1, 1 ], [ 0, 0, 0 ] ], [ [ 1, 1 ], [ 1, 1 ] ],
+		[ [ 1, 1, 1, 1 ], [ 0, 0, 0, 0 ] ] ];
+
+var curr_state_initial = 1;
+var curr_state_moving = 2;
+var curr_state_game_over = 3;
+
 var blockHeight = 4;
 var blockWidth = 4;
 var left = 48;
@@ -5,166 +18,105 @@ var totalWidth = 100;
 var totalHeight = 92;
 
 function initPuzzle() {
-	var board = new PuzzleBoard();
-	createNewPuzzle(board);
+	var game = new TetrisGame();
+	document.addEventListener("keydown", function(e) {
+		onKeyDown(e, game);
+	}, false);
+
+	setInterval(function() {
+		if (game.currentState == curr_state_initial) {
+			game.currentObject = createNewPuzzle();
+			game.currentState = curr_state_moving;
+		} else if (game.currentState == curr_state_moving) {
+			moveDown(game);
+		}
+	}, 500);
 }
 
-function PuzzleBoard() {
-	this.filledBlocks = createEmptyArray(totalWidth / blockWidth, totalHeight
-			/ blockHeight);
+function TetrisGame() {
+	this.currentState = curr_state_initial;
+	this.currentObject = null;
 }
 
-function Puzzle(board) {
-	var that = this;
-	this.puzzleBoard = board;
-	this.colors = [ '#32a4fa', '#38C44F', '#FFAC1C', '#FF6600', '#CC54C4',
-			'#999', '#FF0000' ];
-	this.puzzles = [ [ [ 0, 0, 1 ], [ 1, 1, 1 ], [ 0, 0, 0 ] ],
-			[ [ 1, 0, 0 ], [ 1, 1, 1 ], [ 0, 0, 0 ] ],
-			[ [ 0, 1, 1 ], [ 1, 1, 0 ], [ 0, 0, 0 ] ],
-			[ [ 1, 1, 0 ], [ 0, 1, 1 ], [ 0, 0, 0 ] ],
-			[ [ 0, 1, 0 ], [ 1, 1, 1 ], [ 0, 0, 0 ] ], [ [ 1, 1 ], [ 1, 1 ] ],
-			[ [ 1, 1, 1, 1 ], [ 0, 0, 0, 0 ] ] ];
-
-	this.elements = [];
-	this.previousPosition = [];
-
-	this.createPuzzle = function(puzzle) {
-		var puzzleType = random(this.puzzles.length);
-		var nextPuzzle = this.puzzles[puzzleType];
-		var background = this.colors[random(this.colors.length)];
-
-		for (var y = 0; y < nextPuzzle.length; y++) {
-			for (var x = 0; x < nextPuzzle[y].length; x++) {
-				if (nextPuzzle[y][x]) {
-					var el = document.createElement("div");
-					el.id = "block" + puzzleType;
-					el.className = 'block';
-					el.style.left = (left + (x * blockWidth)) + '%';
-					el.style.top = (y * blockHeight) + '%';
-					el.style.background = background;
-					document.getElementById("tetris-area").appendChild(el);
-					this.elements.push(el);
-				}
+function createNewPuzzle() {
+	var puzzleType = random(this.puzzles.length);
+	var puzzle = puzzles[puzzleType];
+	var background = colors[random(this.colors.length)];
+	var elements = [];
+	for (var y = 0; y < puzzle.length; y++) {
+		for (var x = 0; x < puzzle[y].length; x++) {
+			if (puzzle[y][x]) {
+				var el = document.createElement("div");
+				el.id = "block" + puzzleType;
+				el.className = 'block';
+				el.style.left = (left + (x * blockWidth)) + '%';
+				el.style.top = (y * blockHeight) + '%';
+				el.style.background = background;
+				document.getElementById("tetris-area").appendChild(el);
+				elements.push(el);
 			}
 		}
-
-		var id = setInterval(function() {
-			that.moveDown(id);
-		}, 500);
 	}
+	return elements;
+}
 
-	this.moveDown = function(id) {
-		var moveBlock = this.mayMove();
-		if (moveBlock) {
-			for (var i = 0; i < that.elements.length; i++) {
-				var block = that.elements[i];
-				var blockTop = parseFloat(block.style.top);
+function moveDown(game) {
+	var currentObject = game.currentObject;
+	for (var i = 0; i < currentObject.length; i++) {
+		var block = currentObject[i];
+		var blockTop = parseFloat(block.style.top);
 
-				that.updatePuzzleBoard(block, i);
-
-				if (blockTop <= 92) {
-					block.style.top = (blockTop + blockHeight) + '%';
-				} else {
-					clearInterval(id);
-				}
-			}
-
-		}
-
-		that.clearPreviousPositions();
-
-	}
-
-	this.moveRight = function() {
-		that.clearPreviousPositions();
-		for (var i = 0; i < that.elements.length; i++) {
-			var block = this.elements[i];
-			var blockLeft = parseFloat(block.style.left);
-
-			if (blockLeft <= 100) {
-				block.style.left = (blockLeft + blockWidth) + '%';
-			}
-
-			that.updatePuzzleBoard(block, i);
-
-		}
-	}
-
-	this.moveLeft = function() {
-		that.clearPreviousPositions();
-		for (var i = 0; i < that.elements.length; i++) {
-			var block = this.elements[i];
-			var blockLeft = parseFloat(block.style.left);
-
-			if (blockLeft >= 0) {
-				block.style.left = (blockLeft - blockWidth) + '%';
-			}
-			that.updatePuzzleBoard(block, i);
-		}
-	}
-
-	this.clearPreviousPositions = function() {
-		for (var i = 0; i < that.previousPosition.length; i++) {
-			x = that.previousPosition[i][0];
-			y = that.previousPosition[i][1];
-			that.puzzleBoard.filledBlocks[x][y] = 0;
-		}
-	}
-
-	this.mayMove = function() {
-
-		for (var i = 0; i < that.elements.length; i++) {
-			var block = this.elements[i];
-			var blockLeft = parseFloat(block.style.left);
-			var blockTop = parseFloat(block.style.top);
-
-			x = blockLeft / 4;
-			y = blockTop / 4;
-
-			if (that.puzzleBoard.filledBlocks[x + 1][y + 1] != 0) {
-				return false;
-			}
-
-		}
-
-		return true;
-
-	}
-
-	this.updatePuzzleBoard = function(block, i) {
 		var blockLeft = parseFloat(block.style.left);
 		var blockTop = parseFloat(block.style.top);
 
 		var x = blockLeft / 4;
 		var y = blockTop / 4;
 
-		that.puzzleBoard.filledBlocks[x][y] = block;
-		that.previousPosition[i] = [ x, y ];
+		if (blockTop <= 92) {
+			block.style.top = (blockTop + blockHeight) + '%';
+		} else {
+			game.currentState = curr_state_initial;
+		}
 	}
 }
 
-function createNewPuzzle(board) {
-	var puzzle = new Puzzle(board);
-	document.addEventListener("keydown", function(e) {
-		onKeyDown(e, puzzle);
-	}, false);
-	puzzle.createPuzzle();
-}
-
-function onKeyDown(e, puzzle) {
+function onKeyDown(e, game) {
 	switch (e.keyCode) {
 	case 39: // Right Key
 	{
-		puzzle.moveRight();
+		moveRight(game);
 		break;
 	}
 
 	case 37: // Left Key
 	{
-		puzzle.moveLeft();
+		moveLeft(game);
 		break;
 	}
 
+	}
+}
+
+function moveRight(game) {
+	var currentObject = game.currentObject;
+	for (var i = 0; i < currentObject.length; i++) {
+		var block = currentObject[i];
+		var blockLeft = parseFloat(block.style.left);
+
+		if (blockLeft <= 100) {
+			block.style.left = (blockLeft + blockWidth) + '%';
+		}
+	}
+}
+
+function moveLeft(game) {
+	var currentObject = game.currentObject;
+	for (var i = 0; i < currentObject.length; i++) {
+		var block = currentObject[i];
+		var blockLeft = parseFloat(block.style.left);
+
+		if (blockLeft >= 0) {
+			block.style.left = (blockLeft - blockWidth) + '%';
+		}
 	}
 }
